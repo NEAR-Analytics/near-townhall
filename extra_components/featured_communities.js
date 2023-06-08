8dcc44c63f58cda9537e585406a32feaec9fe222
Yad Konrad <yad.konrad@gmail.com>
2023-06-08 19:40:07 -0400

    more components'

---
+++ extra_components/featured_communities.js
@@ -0,0 +1,17 @@
+{/* <div class="h5 pb-3">Featured Communities</div>
+<div class="row">
+  {Object.entries(communities).map(([label, community]) => {
+    return (
+      <div class="col">
+        {widget(
+          "components.community.FeaturedCommunity",
+          {
+            label,
+            ...community,
+          },
+          label
+        )}
+      </div>
+    );
+  })}
+</div> */}
